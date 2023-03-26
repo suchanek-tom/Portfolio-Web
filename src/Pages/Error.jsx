323f3c94646585793bd8eb178c3a8a0c3449e63e
Tomáš Suchanek <tomassuchanek96@gmail.com>
2023-03-26 23:58:09 +0200

    translate text and add responsive design in Error Page

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -4,21 +4,21 @@ import { NavLink } from "react-router-dom"
 const Error = () => {
   return (
     <div>
-       <div className="px-40 py-20 rounded-md ">
+       <div className="px-40 rounded-md h-screen py-28">
           <div className="flex flex-col items-center">
             <h1 className="font-bold text-yellow-500 text-9xl">404</h1>
 
-            <h6 className="mb-2 text-2xl font-bold text-center text-white md:text-3xl">
-              <span className="text-yellow-500">Oops!</span> Stránka nenalezena
+            <h6 className="mb-2 text-xl font-bold text-center text-white md:text-3xl">
+              <span className="text-yellow-500">Oops!</span> Page was not found
             </h6>
 
-            <p className="mb-8 text-center text-gray-400 md:text-lg">
-              Stránku, kterou hledáte zřejmě neexistuje
+            <p className="mb-8 text-center text-gray-400 text-base md:text-lg">
+              The page you are looking for doesn't exist
             </p>
 
             <a>
-              <NavLink to="/" className="hover:text-yellow-500 duration-500 active:text-yellow-500 text-xl">
-                Zpět
+              <NavLink to="/" className="hover:text-yellow-500 duration-500 active:text-yellow-500 text-2xl">
+                Home
               </NavLink>
             </a>
 
