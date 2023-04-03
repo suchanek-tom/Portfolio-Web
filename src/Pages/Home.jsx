f71dd823395989d8c1e3f97ff98a85d9d452d3e3
Tomáš Suchanek <tomassuchanek96@gmail.com>
2023-04-03 17:14:36 +0200

    ...

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,8 @@
+import React  from "react";
 import Nav from "../components/Nav"
 import { TypeAnimation } from "react-type-animation"
-import Image from "../../dist/assets/Web-designer-31c43f73-31c43f73.webp"
+
+import Image from "../images/Web-designer.webp"
 
 // ICONS
 import { SiGithub } from "react-icons/si";
